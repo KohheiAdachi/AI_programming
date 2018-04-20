8598f5d542dd4286bca8de5f99aed085084e8bad
Kohei ADACHI <s231202k@hikari-1.p.isc.kyutech.ac.jp>
2018-04-20 11:23:28 +0900

    aa

--- report3.pl
+++ report3.pl
@@ -1,6 +1,8 @@
 resistor(power,n1).
 resistor(power,n2).
 resistor(power,n5).
+resistor(power,n7).
+resistor(power,n8).
 
 transistor(n2,gnd,n1).
 transistor(n3,n4,n2).
@@ -13,5 +15,5 @@ transistor(n8,gnd,power).
 inverter(In,Out) :- transistor(In,gnd,Out),resistor(power,Out).
 nand(In1,In2,Out) :- transistor(In1,X,Out),transistor(In2,gnd,X),resistor(power,Out).
 and(In1,In2,Out) :- nand(In1,In2,X),inverter(X,Out).
-nor(In1,In2,Out) :- transistor(In1,gnd,Out),transistor(In2,gnd,Out),resistor(power,Out).
+nor(In1,In2,Out) :- transistor(In1,gnd,Out),transistor(In2,gnd,Out),resistor(power,Out),In1\==In2.
 or(In1,In2,Out) :- nor(In1,In2,X),inverter(X,Out).
