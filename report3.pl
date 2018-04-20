7597637bea44ea7fab32762ad27444376e65d6ae
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-04-20 11:09:24 +0900

    add n7,n8

--- report3.pl
+++ report3.pl
@@ -6,9 +6,12 @@ transistor(n2,gnd,n1).
 transistor(n3,n4,n2).
 transistor(n5,gnd,n4).
 transistor(n6,gnd,n5).
+transistor(n7,gnd,power).
+transistor(n8,gnd,power).
+
 
 inverter(In,Out) :- transistor(In,gnd,Out),resistor(power,Out).
 nand(In1,In2,Out) :- transistor(In1,X,Out),transistor(In2,gnd,X),resistor(power,Out).
 and(In1,In2,Out) :- nand(In1,In2,X),inverter(X,Out).
-nor(In1,In2,Out) :- transistor(In1,gnd,X),transistor(In2,gnd,X),resistor(power,Out).
+nor(In1,In2,Out) :- transistor(In1,gnd,Out),transistor(In2,gnd,Out),resistor(power,Out).
 or(In1,In2,Out) :- nor(In1,In2,X),inverter(X,Out).
