55174d72dd2e36631cb01213494ead60076e1bca
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-04-27 10:42:46 +0900

    add report5

--- report5.pl
+++ report5.pl
@@ -8,6 +8,6 @@ arc(d,c,3).
 %中継点なしの時
 cost(X,Y,C) :- arc(X,Y,C).
 %中継点ありの時
-cost(X,Y,C) :- arc(X,C1,Ans),
+cost(X,Y,C) :- arc(X,C1,D1),
                cost(C1,Y,Ans),
-               C is C1.
+               C is C1 + Ans.
