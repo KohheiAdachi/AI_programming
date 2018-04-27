62ecccdb969fabe8e9e0b9a5f8555b579962270c
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-04-27 10:40:20 +0900

    repo

---
+++ report5.pl
@@ -0,0 +1,13 @@
+%ルートの定義
+arc(a,b,2).
+arc(a,c,5).
+arc(a,d,6).
+arc(b,c,3).
+arc(b,d,1).
+arc(d,c,3).
+%中継点なしの時
+cost(X,Y,C) :- arc(X,Y,C).
+%中継点ありの時
+cost(X,Y,C) :- arc(X,C1,Ans),
+               cost(C1,Y,Ans),
+               C is C1.
