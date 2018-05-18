7ed98ee3eca77b01b83cf80d5dee7c9a754368a1
Kohei ADACHI <s231202k@hikari-1.p.isc.kyutech.ac.jp>
2018-05-18 11:28:43 +0900

    unique

--- report7.pl
+++ report7.pl
@@ -1,24 +1,32 @@
 %member(A,L)の定義
 %単純処理
-member(A,List) :- L = [A | _ ].
+my_member(A,List) :- L = [A | _ ].
 %再帰処理
-member(A,L) :- L = [H | T],
+my_member(A,L) :- L = [H | T],
                   A \= H,
                   member(A,T).
 %append(X,Y,Z)の定義
 %単純処理
-append(X,Y,Z) :- X = [],Z = Y.
+my_append(X,Y,Z) :- X = [],Z = Y.
 %再帰処理
-append(X,Y,Z) :- X = [H|T],
+my_append(X,Y,Z) :- X = [H|T],
                 append(T,Y,Z1),
                 Z = [H|Z1].
 %unique(L,S)
 %単純処理
-
-
+unique(L,S) :- L =[A | []],S = [A].
+%再帰処理
+unique(L,S) :- L = [H|T],
+	       unique(T,S1),
+	       \+member(H,S1), 
+	       S = [H|S1].
+unique(L,S) :- L = [H|T],
+               unique(T,S1),
+	       member(H,S1),
+	       S = S1.
 
 %myRev(L,RL)
 myRev([],RL) :- RL = [].
 myRev(L,RL) :- L = [H|T],
-              append(RL,L,Z1),
-              Z = [H | Z1].
+              myRev(T,RL1),
+              my_append(RL1,[H],RL).
