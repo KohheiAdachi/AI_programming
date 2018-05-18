20e8fb5fdd308dec2c9af1c6f60486d18ca54480
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-05-18 10:29:53 +0900

    myrev

---
+++ report7.pl
@@ -0,0 +1,23 @@
+%member(A,L)の定義
+%単純処理
+member(A,List) :- L = [A | _ ].
+%再帰処理
+member(A,L) :- L = [H | T],A ¥= H,
+                  member(A,T).
+%append(X,Y,Z)の定義
+%単純処理
+append(X,Y,Z) :- X = [],Z = Y.
+%再帰処理
+append(X,Y,Z) :- X = [H|T],
+                append(T,Y,Z1),
+                Z = [H|Z1].
+%unique(L,S)
+%単純処理
+
+
+
+%myRev(L,RL)
+myRev([],RL) :- RL = []
+myRev(L,RL) :- L = [X|T],
+              append(Y,T,Z1),
+              Z = [H | Z1].
