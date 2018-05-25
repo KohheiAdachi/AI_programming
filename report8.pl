e4e2beb0a12e663b0440015969ffc625275c33a7
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-05-25 11:14:29 +0900

    コッ味と

--- report8.pl
+++ report8.pl
@@ -2,3 +2,8 @@
 elm(L,E) :- L = [A|_],E=[A,1].
 elm(L,E) :- L = [_,A|_],E=[A,0].
 elm(L,E) :- L = [_,_|A],elm(A,E).
+
+%combination(S,X):集合Sの要素の組み合わせXを求める
+combination([],[]).
+combination(S,X) :- S = [H|T], combination(S,T).
+combination([H1|T1],[H2|T2]) :- combination(T1,T2).
