f55dcff20b0cbd77635482bced52764af3a73bd1
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-06-08 15:20:10 +0900

    Merge branch 'master' of github.com:KohheiAdachi/AI_programming

---
+++ report9_color.pl
@@ -0,0 +1,35 @@
+%色の定義
+color(red).
+color(while).
+color(blue).
+color(yellow).
+%接している領域の定義
+conn(a,e).
+conn(a,d).
+conn(a,c).
+conn(a,b).
+conn(d,a).
+conn(d,c).
+conn(d,b).
+conn(e,b).
+conn(b,c).
+%逆説対応
+border(X,Y) :- conn(X,Y).
+border(Y,X) :- conn(X,Y).
+%関数本体
+paint([],[]).
+paint(In,Re) :- In = [H|T],
+                paint(T,Re1),
+                subpaint(H,Re).
+                Re = [[H,C]|Re1].
+%sub_paint
+subpaint([],_,C) :- color(C).
+subpaint([[HH,HT]|T],In,C):-
+                          subpaint(T,In,C1),
+                          border(In,HH),
+                          C1 \== HT,
+                          C = C1.
+
+subpaint([[HH,_]|T],In,C) :- subpaint(T,In,C1),
+                            \+border(In,HH),
+                            C=C1.
