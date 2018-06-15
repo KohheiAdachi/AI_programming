67d9d228bb16a62d9ea433d5321e9183190d57cc
Kohei ADACHI <s231202k@its019.p.isc.kyutech.ac.jp>
2018-06-15 16:06:44 +0900

    queen

--- queen.pl
+++ queen.pl
@@ -6,18 +6,26 @@ queen_print(N,Result) :- generate(N,S),queen_acc(N,S,[],Result,[],[]),disp(Resul
 
 % １からNまでの数を要素とするリストを生成する。
 % generate(N,S) : Ｎまでの数字のリストがS
-
-
+generate(0,[]).
+generate(N,S) :- N1 is N-1,
+	         generate(N1,S1),
+                 S = [N|S1].
 
 
 % 与えられたリストの中から要素を一つ選ぶ。
 % select(S,A,Rest) : リストSの中から一つの要素Aをとり出した残りのリストがRest
 %  バックトラックするごとに、別の要素を選択する。
+select([H|T],H,T).
 
+select([H|T],X,Rest) :-
+                      select(T,X,Rest2),
+                      Rest = [H|Rest2].
 
 
 % リストの要素でないことを確認する。
 % notmem(A,L) : AがリストＬの要素でない時成功
+notmem(A,L):- \+member(A,L).
+
 
 
 
@@ -28,9 +36,16 @@ queen_print(N,Result) :- generate(N,S),queen_acc(N,S,[],Result,[],[]),disp(Resul
 %  PosAcc ; queenを配置する列番号用のアキュムレータ
 %  PosResult : queenを配置する列番号の最終結果
 %  WaListAcc : 行番号と列番号の和を蓄えるためのアキュムレータ。最終結果は返さない。
-%  SaListAcc : 行番号と列番号の差を蓄えるためのアキュムレータ。最終結果は返さない。
+				%  SaListAcc : 行番号と列番号の差を蓄えるためのアキュムレータ。最終結果は返さない。
+queen_acc(_,[],PosAcc,PosAcc,_,_).
 
-
+queen_acc(N,S,PosAcc,PosResult,WaListAcc,SaListAcc) :-
+	select(S,A,Rest),
+	N1 is N-1,
+	Wa1 is N+A,Sa1 = N-A,
+	notmem(Wa1,WaListAcc),
+	notmem(Sa1,SaListAcc),
+	queen_acc(N1,Rest,[A|PosAcc],PosResult,[Wa1|WaListAcc],[Sa1|SaListAcc]).
 
 
 
--- report10.pl
+++ report10.pl
@@ -1,21 +1,16 @@
 %ルートの定義
 arc(a,b,2).
-arc(a,c,5).
+arc(c,a,5).
 arc(a,d,6).
-arc(b,c,3).
+arc(c,b,3).
 arc(b,d,1).
 arc(d,c,3).
 
-%中継点なしの時
-%path(X,Y,P,L):-path(X,Y,[],P,0,L).
-%path(X,Y,AccP,P,AccL,L) :- arc(X,Y,L1),
-%                           append(AccP,[X,Y],P),
-%                           L is AccL + L1.
+path(X,Y,P,L) :- path(X,Y,[],P,0,L).
 
-%中継点ありの時
-path(X,Y,AccP,P,AccL,L) :- arc(X,C1,L1),
-                           Y \= C1,
-                           \+member(C1,AccP),
-                           append(AccP,[X],AccPP),
-                           AccLL is AccL + L1,
-                           path(C1,Y,AccPP,P,AccLL,L).
+path(X,X,P,[X|P],L,L).
+
+path(X,Y,AccP,P,AccL,L) :- arc(Z,Y,L1),\+member(Z,AccP),
+                           L2 is AccL+L1,
+                           path(X,Z,[Y|AccP],P,L2,L).
+	
