b72ce4cc20ff454aa32132dcd263cc0950537740
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-06-15 11:43:50 +0900

    変更

--- report10.pl
+++ report10.pl
@@ -5,8 +5,7 @@ arc(a,d,6).
 arc(b,c,3).
 arc(b,d,1).
 arc(d,c,3).
-%単純処理
-path([],[],)
+
 %中継点なしの時
 path(X,Y,P,L):-path(X,Y,[],P,0,L)
 path(X,Y,AccP,P,AccL,L) :- arc(X,Y,L1),
@@ -15,7 +14,7 @@ path(X,Y,AccP,P,AccL,L) :- arc(X,Y,L1),
 
 %中継点ありの時
 path(X,Y,C,P) :- arc(X,C1,L1),
-                 Y \=C1,
+                 Y \= C1,
                  \+member(C1,AccP)
                  append(AccP,[X],AccPP),
                  AccLL is AccL + L1,
