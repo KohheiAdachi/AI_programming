d4071e64f466afaf4ab5d5ae9c46689f5898793d
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-06-15 11:49:11 +0900

    変更

--- report10.pl
+++ report10.pl
@@ -9,13 +9,13 @@ arc(d,c,3).
 %中継点なしの時
 path(X,Y,P,L):-path(X,Y,[],P,0,L).
 path(X,Y,AccP,P,AccL,L) :- arc(X,Y,L1),
-                           append(AccL,[X,Y],P),
+                           append(AccP,[X,Y],P),
                            L is AccL + L1.
 
 %中継点ありの時
-path(X,Y,C,P) :- arc(X,C1,L1),
-                 Y \= C1,
-                 \+member(C1,AccP),
-                 append(AccP,[X],AccPP),
-                 AccLL is AccL + L1,
-                 path(C,B,AccPP,P,AccLL,L).
+path(X,Y,AccP,P,AccL,L) :- arc(X,C1,L1),
+                           Y \= C1,
+                           \+member(C1,AccP),
+                           append(AccP,[X],AccPP),
+                           AccLL is AccL + L1,
+                           path(C,B,AccPP,P,AccLL,L).
