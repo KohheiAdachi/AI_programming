6e3045973767c71fcf1e2a27b33c27b6cf7d3cc6
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-06-15 11:06:23 +0900

    wakaran

--- ex/rev.pl
+++ ex/rev.pl
@@ -1,3 +1,2 @@
 reverse([],[]).    % the empty list is its own reverse. Base for induction.
-reverse([H|T], Rev) :-
-    reverse(T, Trev), append(Trev, [H], Rev).
+reverse([H|T], Rev) :-reverse(T, Trev), append(Trev, [H], Rev).
