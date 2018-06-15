7ddc2284be8d9b854b1846c0dc6510e75a688341
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-06-15 11:38:47 +0900

    ex10

--- report10.pl
+++ report10.pl
@@ -5,10 +5,18 @@ arc(a,d,6).
 arc(b,c,3).
 arc(b,d,1).
 arc(d,c,3).
+%単純処理
+path([],[],)
 %中継点なしの時
-path(X,Y,C,P) :- arc(X,Y,C),P=[X,Y].
+path(X,Y,P,L):-path(X,Y,[],P,0,L)
+path(X,Y,AccP,P,AccL,L) :- arc(X,Y,L1),
+                           append(AccL,X,Y),
+                           L is AccL + L1.
+
 %中継点ありの時
-path(X,Y,C,P) :- arc(X,C1,D1),
-               path(C1,Y,Ans,P1),
-               C is D1 + Ans,
-               P = [X|P1].
+path(X,Y,C,P) :- arc(X,C1,L1),
+                 Y \=C1,
+                 \+member(C1,AccP)
+                 append(AccP,[X],AccPP),
+                 AccLL is AccL + L1,
+                 path(C,B,AccPP,P,AccLL,L).
