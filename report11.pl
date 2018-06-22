6b784a31bb46afe0f5f0fe248e7312b4ec339bd5
Kohei ADACHI <s231202k@its012.p.isc.kyutech.ac.jp>
2018-06-22 15:46:20 +0900

    report11

--- report11.pl
+++ report11.pl
@@ -1,9 +1,16 @@
 select_list([],[],[]).
 
-select_list([H|T],L,O) :- H = [_|_],!,
-                          select_list(T,L1,O),
-                          L = [H|O1].
+select_list([[]|T],[[]|[L]],O) :- !,
+                          select_list(T,L,O).
 
-select_list([H|T],L,O) :- H = [[]|T],!,
-                          select_list(T,L,O1),
-                          O = [[]|O1].
+select_list([[A|B]|T],[[A|B]|L],O) :- !,
+                          select_list(T,L,O).
+select_list([H|T],L,[H|O]) :- !,select_list(T,L,O).
+
+node_n([],0).
+
+node_n([H|T],N):-H=[_|_],!,node_n(H,N1),node_n(T,N2),N is N1+N2.
+
+node_n([_|T],N):-!,node_n(T,N1),N is N1+1.
+
+
