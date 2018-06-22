f2c70ea9ee0e0972acb463d50eb14a2954fcbee9
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-06-22 11:28:40 +0900

    変更

--- report11.pl
+++ report11.pl
@@ -1,9 +1,9 @@
 select_list([],[],[]).
 
-select_list([H|T],O,L) :- H = [_|_],!,
-                          select_list(T,O1,L),
-                          O = [H|O1].
+select_list([H|T],L,O) :- H = [_|_],!,
+                          select_list(T,L1,O),
+                          L = [H|O1].
 
-select_list([H|T],O,L) :- H = [[]|_],!,
-                          select_list(T,O,L1),
-                          L = [H|L1]. 
+select_list([H|T],L,O) :- H = [[]|T],!,
+                          select_list(T,L,O1),
+                          O = [[]|O1].
