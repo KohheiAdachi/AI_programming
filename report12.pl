00459c776f819a5cade6d203d4979503ad3552bb
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2018-06-29 15:32:49 +0900

    単語追加

--- report12.pl
+++ report12.pl
@@ -35,8 +35,15 @@ det([a|X],X,det(a)).
 n([book|X],X,n(book),book).
 n([pen|X],X,n(pen),pen).
 pn([taro|X],X,pn(taro),taro,s3).
-v([has|X],X,v(has),have(S,O),(S,O),[s3]).
-v([have|X],X,v(have),have(S,O),(S,O),[s1,s2,s3,p1,p2,p3]).
+pn([yuki|X],X,pn(yuki),yuki,s3).
+pn([cats|X],X,pn(cats),cats,p3).
+pn([hukuoka|X],X,pn(hukuoka),hukuoka,s3).
+pn([jiro|X],X,pn(jiro),jiro,s3).
+v([has|X],X,v(has),has(S,O),(S,O),[s3]).
+v([like|X],X,v(like),like(S,O),(S,O),[s1,s2,p1,p2,p3]).
+v([likes|X],X,v(likes),likes(S,O),(S,O),[s3]).
+v([lives|X],X,v(lives),lives(S,O),(S,O),[s3]).
+v([have|X],X,v(have),have(S,O),(S,O),[s1,s2,p1,p2,p3]).
 what([what|X],X,what(what),?).
 av([do|X],X,(do),[s1,s2,p1,p2,p3]).
 av([does|X],X,(does),[s3]).
